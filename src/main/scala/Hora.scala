6f3cb4ef0533943740ca2c28e85ab783bde1884e
ale-tasistro <ale.tasistro@gmail.com>
2020-06-22 17:19:26 -0300

    Changed hour print format.

--- src/main/scala/Hora.scala
+++ src/main/scala/Hora.scala
@@ -2,7 +2,7 @@ case class Hora(hora : Int, minuto : Int) {
   require(hora >= 0 && 0 <= minuto && minuto < 60, "Una hora válida, por favor.")
 
   def horaString: String =
-    if (minuto==0) s"$hora : 00"
+    if (minuto < 10) s"$hora : 0$minuto"
     else s"$hora : $minuto"
 
   def respecto(y:Hora): String =
